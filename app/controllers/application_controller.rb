d7f16e471d079f3bfe7402e3f37f13507c37fa0f
Jasmine Carter <jasmine.carter87@gmail.com>
2020-04-11 16:56:59 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -22,7 +22,7 @@ class ApplicationController < Sinatra::Base
 
   post '/recipes' do
     @recipe = Recipe.create(params)
-    redirect "/recipes/#{recipe.id}"
+    redirect "/recipes/#{Recipe.last.id}"
   end
 
   #show specific recipe
