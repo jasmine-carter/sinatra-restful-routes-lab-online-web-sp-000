9104d6b26a34eb8f423fa80f68942e39ebf0a13a
Jasmine Carter <jasmine.carter87@gmail.com>
2020-04-11 18:05:09 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -23,7 +23,6 @@ class ApplicationController < Sinatra::Base
   #receives params from form, creates new recipe, and directs user to show page to see recipe
   post '/recipes' do
     @recipe = Recipe.create(params[:recipe])
-    binding.pry
     redirect "/recipes/#{Recipe.last.id}"
   end
 
